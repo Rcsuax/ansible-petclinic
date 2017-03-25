08d0c4e99bf1f458242662a5513b5015301becf5
Rcsuax <rcsuax@gmail.com>
2017-03-25 16:04:54 +0000

    fix directory

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ansible-playbook --private-key $PWD/.ssh/id_rsa.pub -u vagrant playbook.yml -i hosts
+ansible-playbook --private-key $HOME/.ssh/id_rsa.pub -u vagrant playbook.yml -i hosts
